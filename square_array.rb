97adcadc1487b343eb9cf3d26886bb1cc80b0e8d
DAZUN XING <dazun.xing@gmail.com>
2020-04-01 02:31:39 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
+  counter=0
+  newarray=[]
+  array.each do |i|
+    k=i*i
+    newarray[counter]=k
+    counter+=1
+  end
+  return newarray
+  
   # your code here
+  
 end
