6d055aea5aa4dbccacc0c89edc2abf50adcb7f91
DAZUN XING <dazun.xing@gmail.com>
2020-04-01 06:06:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,7 +7,10 @@ def square_array(array)
     counter+=1
   end
   return newarray
-  
   # your code here
-  
 end
+
+# def square_array(array)
+#   newarray=array.collect {|i| i*i}
+#   # return newarray
+# end
